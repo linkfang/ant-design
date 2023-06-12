b1606c00432cc6c0e5617a3c85dd33ff51df0f5c
Zhou Fang <zhfaxb@syntronic.com>
2023-06-12 11:18:06 -0400

    Refactored validate only demo

--- components/form/demo/validate-only.tsx
+++ components/form/demo/validate-only.tsx
@@ -1,45 +1,40 @@
-import type { FormInstance } from 'antd';
-import { Button, Form, Input, Space } from 'antd';
-import React from 'react';
+import React from "react";
+import { Button, Form, Input, Space } from "antd";
 
-const SubmitButton = ({ form }: { form: FormInstance }) => {
+const App = () => {
+  const [form] = Form.useForm();
   const [submittable, setSubmittable] = React.useState(false);
 
-  // Watch all values
-  const values = Form.useWatch([], form);
-
-  React.useEffect(() => {
+  const validateOnlyFunc = () => {
     form.validateFields({ validateOnly: true }).then(
       () => {
         setSubmittable(true);
       },
       () => {
         setSubmittable(false);
-      },
+      }
     );
-  }, [values]);
+  };
 
   return (
-    <Button type="primary" htmlType="submit" disabled={!submittable}>
-      Submit
-    </Button>
-  );
-};
-
-const App = () => {
-  const [form] = Form.useForm();
-
-  return (
-    <Form form={form} name="validateOnly" layout="vertical" autoComplete="off">
+    <Form
+      form={form}
+      name="validateOnly"
+      layout="vertical"
+      autoComplete="off"
+      onChange={validateOnlyFunc}
+    >
       <Form.Item name="name" label="Name" rules={[{ required: true }]}>
         <Input />
       </Form.Item>
       <Form.Item name="age" label="Age" rules={[{ required: true }]}>
-        <Input />
+        <Input type="number" />
       </Form.Item>
       <Form.Item>
         <Space>
-          <SubmitButton form={form} />
+          <Button type="primary" htmlType="submit" disabled={!submittable}>
+            Submit
+          </Button>
           <Button htmlType="reset">Reset</Button>
         </Space>
       </Form.Item>
